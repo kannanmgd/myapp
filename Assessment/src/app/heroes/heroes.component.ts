8518be86ac5afeefebcf520ac4fce587b86b6b25
Kannan K <kkarunanithi@dminc.com>
2019-07-21 12:53:06 +0530

    working on services directive in heroes app

--- Assessment/src/app/heroes/heroes.component.ts
+++ Assessment/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +10,26 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  constructor(private heroservice: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   hero: Hero = { 
     id: 1,
     name: 'Kannan K'
   }
-  constructor() { }
+  heroes: Hero[];
+  
+  selectedHero: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroservice.getHeroes()
+        .subscribe( heroes => this.heroes = heroes );
   }
 
 }
